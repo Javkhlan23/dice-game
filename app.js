f43dc4e0a127e6b6667efbcfbc6880839332adf3
javkhaa <javhaajavhaa@gmail.com>
2020-04-06 15:21:43 +0800

    toglogchiin eeljiig solidog bolgh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадаглах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 // тоглогчдийн цуглууулсан оноог хадгалах хувьсагч
 
@@ -21,17 +21,54 @@ var diceNumber = Math.floor(Math.random() * 7);
 document.querySelector(".dice").style.display = "none";
 
 document.getElementById("score-0").textContent = "0";
-
+// програм эхлэхэд бэлтгэе
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
+
+// Шоог шидэх эвент листенер
 document
   .querySelector(".btn-roll")
   .addEventListener("click", function shooShid() {
+    //  1 - 6 доторх санамсаргүй нэг тоог гаргаж авна
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    //   шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.style.display = "block";
+
+    // буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.src = "dice-" + diceNumber + ".png";
-    // alert("Шоог шидлээ  : " + diceNumber);
+
+    // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+
+    if (diceNumber !== 1) {
+      // 1-ээс ялгаатай тоо буулаа. буусан тоог тоглогчид нэмж өгнө
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // 1 буусан тул тоглогчин ээлжийг энэ хэсэгт сольж өгнө
+
+      // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+      roundScore = 0;
+      document.getElementById("current-" + activePlayer).textContent = 0;
+      //  хэрэв идэхвтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+      //  үгүй бол идэвхтэй тоглогчийг 0 болго.
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      // if (activePlayer === 0) {
+      //   activePlayer = 1;
+      // } else {
+      //   activePlayer = 1;
+      // }
+
+      // Улаан цэгийг шилжүүлэх
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector(".player-1-panel").classList.toggle("active");
+
+      // Шоог түр арилгана.
+      diceDom.style.display = "none";
+    }
   });
-// console.log("Шоо: " + dice);
